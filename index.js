d44bc622d7453509be502c283fd6b3259b54403c
Erik Gillespie <erik.gillespie@gmail.com>
2020-02-28 13:45:25 -0500

    Newsletter list-o-matic

---
+++ index.js
@@ -0,0 +1,31 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyCMw8mZ1D1GAYpeotAqVCaYAMtn3URVOok',
+  authDomain: 'lansing-codes.firebaseapp.com',
+  databaseURL: 'https://lansing-codes.firebaseio.com',
+  projectId: 'lansing-codes',
+  storageBucket: 'lansing-codes.appspot.com',
+  messagingSenderId: '647280182517',
+  appId: '1:647280182517:web:779f72d0b90c0dd4'
+}
+const firestore = firebase.initializeApp(firebaseConfig).firestore()
+
+const firstOfNextMonth = moment().add(0, 'month').startOf('month').startOf('day')
+const endOfNextMonth = moment(firstOfNextMonth).endOf('month').endOf('day')
+const eventList = document.getElementById('event-list')
+
+firestore
+  .collection('events')
+  .where('startTime', '>=', firstOfNextMonth.valueOf())
+  .where('startTime', '<=', endOfNextMonth.valueOf())
+  .orderBy('startTime', 'asc')
+  .get()
+  .then(snapshot => {
+    snapshot.forEach(doc => {
+      const event = doc.data()
+      const when = moment(event.startTime).format('ddd. MMM. D [at] h:mm')
+      const link = `<a href="${event.url}">${event.name}</a>`
+      const li = document.createElement('li')
+      li.innerHTML = `${when} - ${link}`
+      eventList.appendChild(li)
+    })
+  })
