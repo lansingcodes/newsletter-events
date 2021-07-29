440228801e544557943f3e12c4d2f0aeaf447aca
Erik Gillespie <erik.gillespie@gmail.com>
2021-07-29 16:30:04 -0400

    Adjust formatting to follow Standard linting rules

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/* global firebase, moment */
 const firebaseConfig = {
   apiKey: 'AIzaSyCMw8mZ1D1GAYpeotAqVCaYAMtn3URVOok',
   authDomain: 'lansing-codes.firebaseapp.com',
@@ -14,27 +15,27 @@ function compileEventList (monthMoment, nameId, listId) {
   const endOfMonth = moment(firstOfMonth).endOf('month').endOf('day')
   const eventListEl = document.getElementById(listId)
   const monthNameEl = document.getElementById(nameId)
-  
+
   monthNameEl.innerHTML = firstOfMonth.format('MMMM')
-  
+
   firestore
-  .collection('events')
-  .where('startTime', '>=', firstOfMonth.valueOf())
-  .where('startTime', '<=', endOfMonth.valueOf())
-  .orderBy('startTime', 'asc')
-  .get()
-  .then(snapshot => {
-    snapshot.forEach(doc => {
-      const event = doc.data()
-      const when = moment(event.startTime)
-        .format('ddd. MMM. D [at] h:mm a')
-        .replace(':00', '')
-      const link = `<a href="${event.url}">${event.name}</a>`
-      const li = document.createElement('li')
-      li.innerHTML = `<code>${when}</code> - ${link}`
-      eventListEl.appendChild(li)
+    .collection('events')
+    .where('startTime', '>=', firstOfMonth.valueOf())
+    .where('startTime', '<=', endOfMonth.valueOf())
+    .orderBy('startTime', 'asc')
+    .get()
+    .then(snapshot => {
+      snapshot.forEach(doc => {
+        const event = doc.data()
+        const when = moment(event.startTime)
+          .format('ddd. MMM. D [at] h:mm a')
+          .replace(':00', '')
+        const link = `<a href="${event.url}">${event.name}</a>`
+        const li = document.createElement('li')
+        li.innerHTML = `<code>${when}</code> - ${link}`
+        eventListEl.appendChild(li)
+      })
     })
-  })
 }
 
 compileEventList(moment().add(1, 'month'), 'next-month-name', 'next-month-event-list')
